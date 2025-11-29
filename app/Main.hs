d001c51fa070b24ec9df749a5830f66d7416fc01
jeremytey <jeremy8598@gmail.com>
2025-11-29 22:52:53 +0800

    define date type for question 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+-- Only store what we need for Question 1: Destination Country
+data Booking = Booking
+  { destinationCountry :: String
+  } deriving (Show, Eq)
+
+
+
+
+
+
+
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
